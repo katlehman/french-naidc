7a8678c1143ca6b0869c8233486f2712ff3110ce
katlehman <k.lehman@mac.com>
2020-02-27 23:12:14 -0500

    First commit french-naidc site

---
+++ js/javal.js
@@ -0,0 +1,3 @@
+$(window).load(function(){
+    $(".demo1").lettering("words").children('span').lettering();
+ });
---
+++ js/javaposter.js
@@ -0,0 +1,3 @@
+$(window).load(function(){
+  $(".demo3").lettering("lines");
+});
